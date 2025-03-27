ae109dd6404ac9762b90c3c124222b0438081ec6
Qqkyu <kulmisiu007@gmail.com>
2025-03-27 16:43:46 +0100

    17. Letter Combinations of a Phone Number

---
+++ leetcode/17-letter-combinations-of-a-phone-number/solution.js
@@ -0,0 +1,16 @@
+const DIGIT_LETTER_MAP = {
+  2: ["a", "b", "c"],
+  3: ["d", "e", "f"],
+  4: ["g", "h", "i"],
+  5: ["j", "k", "l"],
+  6: ["m", "n", "o"],
+  7: ["p", "q", "r", "s"],
+  8: ["t", "u", "v"],
+  9: ["w", "x", "y", "z"],
+};
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+function letterCombinations(digits) {}
