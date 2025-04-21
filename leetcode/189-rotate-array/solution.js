568419538e738aa58d4049b7ecbde44e98fa7d7c
Qqkyu <kulmisiu007@gmail.com>
2025-04-21 11:55:01 +0200

    189. Rotate Array

--- leetcode/189-rotate-array/solution.js
+++ leetcode/189-rotate-array/solution.js
@@ -3,12 +3,12 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
-  k %= nums.length;
+function rotate(nums, k) {
+  k = k % nums.length;
   reverse(nums, 0, nums.length - 1);
   reverse(nums, 0, k - 1);
   reverse(nums, k, nums.length - 1);
-};
+}
 
 function reverse(nums, i, j) {
   while (i < j) {
